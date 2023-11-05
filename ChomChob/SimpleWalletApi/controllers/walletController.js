091924955bada741c991a520c07aa9dd396b12a8
ymn-dev <sornniyom.a@gmail.com>
2023-11-05 14:27:09 +0700

    transfer money done

--- ChomChob/SimpleWalletApi/controllers/walletController.js
+++ ChomChob/SimpleWalletApi/controllers/walletController.js
@@ -20,7 +20,7 @@ export const adminAddMoney = async (req, res, next) => {
     const value = [newWalletId, user_id, Number(currency_id), amount ? Number(amount) : 0];
     try {
       await connection.query(query, value);
-      res.json({ message: `Successfully created the wallet ${newWalletId}` });
+      res.json({ message: `Successfully created and add money to the wallet` });
     } catch (err) {
       res.status(400).json({ error: err.sqlMessage });
     } finally {
@@ -30,5 +30,47 @@ export const adminAddMoney = async (req, res, next) => {
 };
 
 export const walletTransaction = async (req, res, next) => {
-  const {} = req.body;
+  const { from_wallet, to_user, to_currency, amount } = req.body;
+  if (amount <= 0) {
+    //take amount from wallet currency then push through exchange later
+    res.status(400).json({ error: "Cannot transfer amount 0 or lower" });
+  }
+  const payerQuery = `SELECT * from wallet where wallet_id = ?`;
+  try {
+    const payerWallet = await connection.query(payerQuery, [from_wallet]);
+    const payerBalance = Number(payerWallet[0].balance) || 0;
+    if (payerWallet[0].currency_id === to_currency) {
+      if (Number(amount) > payerBalance) {
+        res.status(400).json({ error: "You don't have the required amount in your wallet" });
+        return;
+      }
+    }
+    const getExchangeRateQuery = `SELECT * FROM exchange_rate where from_currency = ? AND to_currency = ?`;
+    const getExchangeRate = await connection.query(getExchangeRateQuery, [payerWallet[0].currency_id, Number(to_currency)]);
+    const exchangeRate = getExchangeRate.length > 0 ? Number(getExchangeRate[0].rate) : 1;
+    const transferAmount = Number(amount) * exchangeRate;
+    if (transferAmount > payerBalance * exchangeRate) {
+      res.status(400).json({ error: "You don't have the required amount in your wallet" });
+      return;
+    }
+
+    const receiverQuery = `SELECT * from wallet where user_id = ? AND currency_id = ?`;
+    const receiverWallet = await connection.query(receiverQuery, [to_user, Number(to_currency)]);
+    const updatePayerWalletQuery = `UPDATE wallet SET balance = ? WHERE wallet_id = ?;`;
+    const updatePayerWallet = await connection.query(updatePayerWalletQuery, [payerBalance - Number(amount), from_wallet]);
+    if (receiverWallet.length === 0) {
+      const newWalletId = crypto.randomUUID();
+      const query = `INSERT into wallet (wallet_id, user_id, currency_id, balance) VALUES (?, ?, ?, ?)`;
+      const value = [newWalletId, to_user, to_currency, transferAmount];
+      await connection.query(query, value);
+    } else {
+      const receivingQuery = "UPDATE wallet SET balance = balance + ? WHERE wallet_id = ?;";
+      await connection.query(receivingQuery, [transferAmount, receiverWallet[0].wallet_id]);
+    }
+    res.json({ message: "Successfully transferred money" });
+  } catch (err) {
+    res.status(400).json({ error: err.sqlMessage });
+  } finally {
+    connection.release();
+  }
 };
